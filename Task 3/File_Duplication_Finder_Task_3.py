716668adb4ec87faaaf06ff566506bbd6c4b2467
R.S .V <vstudy909@gmail.com>
2025-01-31 23:29:32 +0000

    Subroutine main() has been added to the program.

--- Task 3/File_Duplication_Finder_Task_3.py
+++ Task 3/File_Duplication_Finder_Task_3.py
@@ -8,16 +8,20 @@ import logging
 
 def compute_sha256(file_path):
     hasher = hashlib.sha256()
-
-    with open(file_path, "rb") as f: # Open the file in binary mode.
-        while True:
-            chunk = f.read(4096) # Read files in chunks of 4096 bytes
-            hasher.update(chunk) #Update the hash with file content
+    try:
+        with open(file_path, "rb") as f: # Open the file in binary mode.
+            while True:
+                chunk = f.read(4096) # Read files in chunks of 4096 bytes
+                hasher.update(chunk) #Update the hash with file content
+    except Exception as e:
+        logging.error(f"Could not read {file_path} - {e}")
+        return None
     
     return hasher.hexdigest() # Return the final hash value as a hexadecimal string
 
 def find_duplicates(directory):
     file_hashes = {} #Dictionary to store the hash --> List of file paths
+    duplicates = {}
 
     for root, _, files in os.walk(directory): #walk through the directory
 # os.walk(directory) --> scans through all the files in in the given directory, including subdirectories. Helps us loop through them without manually listing them.
@@ -29,9 +33,15 @@ def find_duplicates(directory):
                 file_hashes[file_hash].append(file_path) #add to existing file
             else:
                 file_hashes[file_hash] = [file_path] #create new list
-    
+
+        for file_hash, file_list in file_hashes.items():
+            if len(file_list) > 1:
+                duplicates[file_hash] = file_list
+
+        return duplicates
+
     # Return only hashes that have more than one file (duplicates)
-    return {h: p for h, p in file_hashes.items() if len(p)>1}
+    # return {h: p for h, p in file_hashes.items() if len(p)>1}
 
 # root gives us the current directory and file is the name of file inside it.
 
@@ -44,6 +54,7 @@ logging.basicConfig(filename="duplicate_files.log", level=logging.INFO, format="
     #Logs deleted files into "duplicate_files.log".
     #Format → Timestamp - Deleted File Path.
     #Helps track which files were deleted for record-keeping.
+
 
 def manual_deletion(duplicates):
     for file_hash, file_list in duplicates.items():
@@ -67,7 +78,7 @@ def manual_deletion(duplicates):
             for i in indexes_to_delete:
                 if 0 <= i < len(file_list):
                     file_to_delete = file_list[i]
-                    os.remove(file_to_delete)
+                    os.remove(file_to_delete) # Delete file.
 
                     logging.info(f"Deleted: {file_to_delete}")
                     print(f"Deleted: {file_to_delete}")
@@ -77,3 +88,26 @@ def manual_deletion(duplicates):
         
         except ValueError:
             print("Invalid input. Please enter numbers seperated by commas.")
+
+# def log_script_completion(total_duplicates):
+#     logging.info(f"Duplicate file scan completed. Total duplicate groups: {total_duplicates}")
+
+def main():
+    directory = input("Enter the directory to scan for duplicates: ").strip()
+
+    if not os.path.isdir(directory):
+        print("Invalid Directory. Please enter a valid path.")
+        logging.error(f"Invalid directory entered: {directory}")
+        return
+    duplicates = find_duplicates(directory)
+
+    if duplicates:
+        print("\nDuplicate files found!")
+        logging.info(f"DUplicate scan completed. Total groups found: {len(duplicates)}")
+        manual_deletion(duplicates)
+    else:
+        print("No duplicates found.")
+        logging.info("No Duplicate files found.")
+
+if __name__ == "__main__":
+    main()
